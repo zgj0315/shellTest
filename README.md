b33a8943ac00497c9de939846d39aeb263e052f1
Zhao Guangjian <zgj0315@gmail.com>
2017-08-15 11:23:01 +0800

    some change

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 本项目主要涉及shell的常用操作记录，以备工程使用
 # 2. 列表
 1. [读取shell脚本自身信息](./readSelfInfo.sh)
+2. [读取文件信息](./readFileInfo.sh)
--- readSelfInfo.sh
+++ readSelfInfo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Created by zhaogj on 20170815
-PRG="$0"
-echo "PRG=$PRG"
-BIN=`cd $(dirname "$PRG"); pwd`
-echo "BIN=$BIN"
-#TMP=$(dirname "$PRG")
-#echo "shell path="$TMP
+# 获取命令字符串
+CMD="$0"
+echo "CMD=$CMD"
+# 获取命令绝对路径
+SHELL_PATH=`cd $(dirname "$CMD"); pwd`
+echo "SHELL_PATH=$SHELL_PATH"
+# 去掉最右侧的/后面的字符
+LAST_PATH=${SHELL_PATH%/*}
+# 去掉最右侧的/前面的字符
+LAST_PATH=${LAST_PATH##*/}
+echo "LAST_PATH=${LAST_PATH}"
