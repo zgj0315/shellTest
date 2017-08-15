966a01070f560afffeda3020c8b8d03f4ae06b1b
Zhao Guangjian <zgj0315@gmail.com>
2017-08-15 13:45:49 +0800

    some change

--- readFileInfo.sh
+++ readFileInfo.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # Created by zhaogj on 20170815
-PRG="$0"
-echo "PRG=$PRG"
-BIN=`cd $(dirname "$PRG"); pwd`
-echo "BIN=$BIN"
-#TMP=$(dirname "$PRG")
-#echo "shell path="$TMP
+
+# 获取命令字符串
+CMD="$0"
+echo "CMD=$CMD"
+# 获取命令绝对路径
+SHELL_PATH=`cd $(dirname "$CMD"); pwd`
+
+for file in ${SHELL_PATH}/*
+do
+    if test -f $file; then
+	SUFFIX=${file##*.}
+	if test $SUFFIX = "sh"; then
+	    echo $file is sh
+	    echo file name ${file##*/}
+	fi
+    fi
+done
